e542965a0d12bc215a7af159b16a874aa78da171
Thiet Nguyen <nguyenthiet1225@gmail.com>
2022-01-07 07:18:31 +0700

    new pj

---
+++ 06_01_22_02.js
@@ -0,0 +1,9 @@
+function run() {
+    let x = parseInt(document.getElementById("age").value)
+    if (x >= 15) {
+        alert("Đã đủ tuổi vào lớp 10")
+    } else {
+        alert("Chưa đủ tuổi vào lớp 10")
+    }
+}
+
